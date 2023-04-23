9bdc6fb86593d894f5a30ec8befa746785ac910d
Kevin Rohlf <123676073+KevinRohlf@users.noreply.github.com>
2023-04-23 17:01:36 +0200

    added fonts & add pick a card function

---
+++ src/models/game.ts
@@ -0,0 +1,27 @@
+export class Game {
+    public players: string[] = ['Hans', 'Peter', 'Freddi'];
+    public stack: string[] = [];
+    public playedCards: string[] = [];
+    public currentPlayer: number = 0;
+
+    constructor() {
+        for(let i = 1; i < 14; i++) {
+            this.stack.push('ace_' + i);
+            this.stack.push('clubs_' + i);
+            this.stack.push('diamonds_' + i);
+            this.stack.push('hearts_' + i);
+        };
+
+        shuffle(this.stack);
+    }
+
+
+    
+}
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
